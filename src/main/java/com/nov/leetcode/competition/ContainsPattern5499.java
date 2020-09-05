aa6608e27c1e21e4929160a21f198847e4da5ec4
longwenhe <longwenhe1990@163.com>
2020-09-05 11:33:27 +0800

    补提交

--- src/main/java/com/nov/leetcode/competition/ContainsPattern5499.java
+++ src/main/java/com/nov/leetcode/competition/ContainsPattern5499.java
@@ -6,4 +6,40 @@ package com.nov.leetcode.competition;
  * @description
  */
 public class ContainsPattern5499 {
+    public boolean containsPattern(int[] arr, int m, int k) {
+        int max = 1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i; j < arr.length; j += m) {
+                int tmp = j;
+                int last = j + (m << 1);
+                if (last > arr.length) {
+                    continue;
+                }
+                int n = j + m;
+                for (; n < last; n++) {
+                    if (arr[tmp] != arr[n]) {
+                        break;
+                    }
+                    tmp++;
+                }
+                if (n == last) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            max = Math.max(max, count);
+        }
+        return max >= k;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 2, 1, 3};
+        int m = 2;
+        int k = 3;
+        ContainsPattern5499 solution = new ContainsPattern5499();
+        System.out.println(solution.containsPattern(arr, m, k));
+    }
 }
--- src/main/java/com/nov/niuke/interview/StringCompare.java
+++ src/main/java/com/nov/niuke/interview/StringCompare.java
@@ -6,4 +6,46 @@ package com.nov.niuke.interview;
  * @description
  */
 public class StringCompare {
-}
+    public static void main(String[] args) {
+        String s1 = "asdf aaa";
+        String s2 = "aSdf     aaa";
+        if (isEqual(s1, s2)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isEqual(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return true;
+        }
+        if (s1 == null && s2 != null) {
+            return false;
+        }
+        if (s1 != null && s2 == null) {
+            return false;
+        }
+        return getSubString(s1).equals(getSubString(s2));
+    }
+
+    public static String getSubString(String s) {
+        char[] chars = s.toCharArray();
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                continue;
+            } else if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] = (char) (chars[i] - 'A' + 'a');
+            }
+            count++;
+        }
+        char[] result = new char[count];
+        int j = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != ' ') {
+                result[j] = chars[i];
+                j++;
+            }
+        }
+        return new String(result); }}
